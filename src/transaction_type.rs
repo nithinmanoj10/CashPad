b12044844238c38c7ea9531e1f181763da26585e
nithinmanoj10 <nithinp.manoj@gmail.com>
2022-08-30 16:03:21 +0530

    ➕ Add new Transaction Type Added

--- src/transaction_type.rs
+++ src/transaction_type.rs
@@ -6,9 +6,35 @@
 // 2. ATM Card
 // 3. GPay , etc
 
+// TODO: Add remove transaction type
+
 use crate::display_utils;
 use colored::Colorize;
 use std::fs;
+use std::io;
+use std::io::Write;
+use text_io::read;
+
+#[derive(Debug)]
+struct TransactionTypeRecord {
+    id: i32,
+    type_name: String,
+    emoticon: String,
+}
+
+impl TransactionTypeRecord {
+    fn read_transaction_type_details(&mut self) {
+        self.id = get_transaction_type_count() + 1;
+
+        print!("{}: ", "Enter Transaction Type Name".yellow().bold());
+        io::stdout().flush().unwrap();
+        self.type_name = read!("{}\n");
+
+        print!("{}: ", "Enter Emoticon".yellow().bold());
+        io::stdout().flush().unwrap();
+        self.emoticon = read!("{}\n");
+    }
+}
 
 pub fn display_transaction_type_menu() {
     // Displays the main menu screen for the
@@ -62,8 +88,42 @@ pub fn transaction(option: i32) {
     match option {
         1 => {
             // View all Transaction Types
-            println!("View all Transaction Types\n");
+            println!("{}\n", "View all Transaction Types".cyan().bold());
             display_transaction_type_table();
+            display_utils::display_go_back_message();
+        }
+
+        2 => {
+            // Add new Transaction Type
+            println!("{}\n", "Add new Transaction Type".cyan().bold());
+
+            let mut db_transaction_type = fs::OpenOptions::new()
+                .write(true)
+                .append(true)
+                .open("./src/data/db_transaction_type.csv")
+                .unwrap();
+
+            let mut new_transaction_type: TransactionTypeRecord = TransactionTypeRecord {
+                id: 0,
+                type_name: String::from(""),
+                emoticon: String::from(""),
+            };
+
+            new_transaction_type.read_transaction_type_details();
+
+            write!(
+                db_transaction_type,
+                "{}",
+                format!(
+                    "\n{},{},{}",
+                    new_transaction_type.id,
+                    new_transaction_type.type_name,
+                    new_transaction_type.emoticon
+                )
+            )
+            .expect("Can't find db_transaction_type.csv");
+
+            println!("\n{}", "Transaction Type successfully added".green().bold());
             display_utils::display_go_back_message();
         }
 
@@ -72,4 +132,14 @@ pub fn transaction(option: i32) {
             display_utils::display_go_back_message();
         }
     }
+}
+
+fn get_transaction_type_count() -> i32 {
+    // Returns the number of transaction type records
+    // in the transaction type database i.e db_transaction_type.csv
+    let transaction_type_contents = fs::read_to_string("./src/data/db_transaction_type.csv")
+        .expect("Can't open file db_transaction_type.csv");
+    let transaction_type_lines: Vec<&str> = transaction_type_contents.lines().collect();
+
+    return (transaction_type_lines.len() - 1).try_into().unwrap();
 }
